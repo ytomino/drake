f5871d00a553ff458e34c988d0ba38df9acc21a5
yt <demoonlit@panathenaia.halfmoon.jp>
2011-03-10 18:02:46 +0900

    Add new implementation unit System.Reference_Counting

---
+++ source/s-refcou.adb
@@ -0,0 +1,213 @@
+package body System.Reference_Counting is
+
+   procedure sync_add_and_fetch_32 (
+      A1 : not null access Counter;
+      A2 : Counter);
+   pragma Import (Intrinsic, sync_add_and_fetch_32, "__sync_add_and_fetch_4");
+
+   function sync_sub_and_fetch_32 (
+      A1 : not null access Counter;
+      A2 : Counter)
+      return Counter;
+   pragma Import (Intrinsic, sync_sub_and_fetch_32, "__sync_sub_and_fetch_4");
+
+   function sync_bool_compare_and_swap (
+      A1 : not null access Natural;
+      A2 : Natural;
+      A3 : Natural)
+      return Boolean;
+   function sync_bool_compare_and_swap (
+      A1 : not null access Natural;
+      A2 : Natural;
+      A3 : Natural)
+      return Boolean
+   is
+      pragma Compile_Time_Error (
+         Integer'Size /= 32 and then Integer'Size /= 64,
+         "Integer'Size is neither 32 nor 64");
+   begin
+      pragma Warnings (Off);
+      if Integer'Size = 32 then
+         pragma Warnings (On);
+         declare
+            function sync_bool_compare_and_swap_32 (
+               A1 : not null access Natural;
+               A2 : Integer;
+               A3 : Integer)
+               return Boolean;
+            pragma Import (Intrinsic, sync_bool_compare_and_swap_32,
+               "__sync_bool_compare_and_swap_4");
+         begin
+            return sync_bool_compare_and_swap_32 (A1, A2, A3);
+         end;
+      else
+         pragma Warnings (On);
+         declare
+            function sync_bool_compare_and_swap_64 (
+               A1 : not null access Natural;
+               A2 : Integer;
+               A3 : Integer)
+               return Boolean;
+            pragma Import (Intrinsic, sync_bool_compare_and_swap_64,
+               "__sync_bool_compare_and_swap_8");
+         begin
+            return sync_bool_compare_and_swap_64 (A1, A2, A3);
+         end;
+      end if;
+      pragma Warnings (On);
+   end sync_bool_compare_and_swap;
+
+   function Shared (Count : Counter) return Boolean is
+   begin
+      return Count > 1; -- static is True
+   end Shared;
+
+   --  not null (using sentinel)
+
+   procedure Adjust (Reference_Count : not null access Counter) is
+   begin
+      if Reference_Count.all /= Static then
+         sync_add_and_fetch_32 (Reference_Count, 1);
+      end if;
+   end Adjust;
+
+   procedure Assign (
+      Target : not null access Address;
+      Target_Reference_Count : not null access Counter;
+      Source : not null access constant Address;
+      Source_Reference_Count : not null access Counter;
+      Free : not null access procedure (Object : Address)) is
+   begin
+      if Target.all /= Source.all then
+         Clear (Target, Target_Reference_Count, Free);
+         Target.all := Source.all;
+         Adjust (Source_Reference_Count);
+      end if;
+   end Assign;
+
+   procedure Clear (
+      Target : not null access Address;
+      Reference_Count : not null access Counter;
+      Free : not null access procedure (Object : Address)) is
+   begin
+      if Reference_Count.all /= Static then
+         if sync_sub_and_fetch_32 (Reference_Count, 1) = 0 then
+            Free (Target.all);
+         end if;
+      end if;
+   end Clear;
+
+   procedure Move (
+      Target : not null access Address;
+      Target_Reference_Count : not null access Counter;
+      Source : not null access Address;
+      Sentinel : Address;
+      Free : not null access procedure (Object : Address)) is
+   begin
+      if Target.all /= Source.all then
+         Clear (Target, Target_Reference_Count, Free);
+         Target.all := Source.all;
+         Source.all := Sentinel;
+      end if;
+   end Move;
+
+   procedure Unique (
+      Target : not null access Address;
+      Target_Reference_Count : not null access Counter;
+      Target_Length : Natural;
+      Target_Capacity : Natural;
+      Max_Length : Natural;
+      Capacity : Natural;
+      Sentinel : Address;
+      Copy : not null access procedure (
+         Target : out Address;
+         Source : Address;
+         Length : Natural;
+         Max_Length : Natural;
+         Capacity : Natural);
+      Free : not null access procedure (Object : Address)) is
+   begin
+      if Capacity /= Target_Capacity
+         or else ( -- static (excluding Sentinel) is True
+            Target.all /= Sentinel
+            and then Target_Reference_Count.all > 1)
+      then
+         declare
+            Old : aliased Address := Target.all;
+            New_Capacity : constant Natural :=
+               Integer'Max (Capacity, Target_Length);
+         begin
+            if New_Capacity = 0 then
+               Target.all := Sentinel;
+            else
+               Copy (
+                  Target.all,
+                  Old,
+                  Target_Length,
+                  Max_Length,
+                  New_Capacity);
+            end if;
+            Clear (Old'Access, Target_Reference_Count, Free => Free);
+         end;
+      end if;
+   end Unique;
+
+   procedure Set_Length (
+      Target : not null access Address;
+      Target_Reference_Count : not null access Counter;
+      Target_Length : Natural;
+      Target_Max_Length : not null access Natural;
+      Target_Capacity : Natural;
+      New_Length : Natural;
+      Sentinel : Address;
+      Copy : not null access procedure (
+         Target : out Address;
+         Source : Address;
+         Length : Natural;
+         Max_Length : Natural;
+         Capacity : Natural);
+      Free : not null access procedure (Object : Address)) is
+   begin
+      if New_Length > Target_Length then
+         if New_Length > Target_Capacity then
+            --  expanding
+            declare
+               New_Capacity : constant Natural :=
+                  Integer'Max (Target_Capacity * 2, New_Length);
+            begin
+               Unique (
+                  Target => Target,
+                  Target_Reference_Count => Target_Reference_Count,
+                  Target_Length => Target_Length,
+                  Target_Capacity => Target_Capacity,
+                  Max_Length => New_Length,
+                  Capacity => New_Capacity,
+                  Sentinel => Sentinel,
+                  Copy => Copy, -- Copy should set Max_Length
+                  Free => Free);
+            end;
+         else
+            --  try to use reserved area
+            if sync_bool_compare_and_swap (
+               Target_Max_Length,
+               Target_Length,
+               New_Length)
+            then
+               null;
+            elsif Target_Reference_Count.all > 1 then
+               Unique (
+                  Target => Target,
+                  Target_Reference_Count => Target_Reference_Count,
+                  Target_Length => Target_Length,
+                  Target_Capacity => Target_Capacity,
+                  Max_Length => New_Length,
+                  Capacity => Target_Capacity, -- keep Capacity
+                  Sentinel => Sentinel,
+                  Copy => Copy, -- Copy should set Max_Length
+                  Free => Free);
+            end if;
+         end if;
+      end if;
+   end Set_Length;
+
+end System.Reference_Counting;
---
+++ source/s-refcou.ads
@@ -0,0 +1,67 @@
+pragma License (Unrestricted);
+--  implementation package
+package System.Reference_Counting is
+   pragma Pure;
+
+   type Counter is mod 2 ** 32;
+   for Counter'Size use 32;
+
+   Static : constant Counter := Counter'Last;
+
+   function Shared (Count : Counter) return Boolean;
+
+   procedure Adjust (
+      Reference_Count : not null access Counter);
+
+   procedure Assign (
+      Target : not null access Address;
+      Target_Reference_Count : not null access Counter;
+      Source : not null access constant Address;
+      Source_Reference_Count : not null access Counter;
+      Free : not null access procedure (Object : Address));
+
+   procedure Clear (
+      Target : not null access Address;
+      Reference_Count : not null access Counter;
+      Free : not null access procedure (Object : Address));
+
+   procedure Move (
+      Target : not null access Address;
+      Target_Reference_Count : not null access Counter;
+      Source : not null access Address;
+      Sentinel : Address;
+      Free : not null access procedure (Object : Address));
+
+   procedure Unique (
+      Target : not null access Address;
+      Target_Reference_Count : not null access Counter;
+      Target_Length : Natural;
+      Target_Capacity : Natural;
+      Max_Length : Natural;
+      Capacity : Natural;
+      Sentinel : Address;
+      Copy : not null access procedure (
+         Target : out Address;
+         Source : Address;
+         Length : Natural;
+         Max_Length : Natural;
+         Capacity : Natural);
+      Free : not null access procedure (Object : Address));
+
+   procedure Set_Length (
+      Target : not null access Address;
+      Target_Reference_Count : not null access Counter;
+      Target_Length : Natural;
+      Target_Max_Length : not null access Natural;
+      Target_Capacity : Natural;
+      New_Length : Natural;
+      Sentinel : Address;
+      Copy : not null access procedure (
+         Target : out Address;
+         Source : Address;
+         Length : Natural;
+         Max_Length : Natural;
+         Capacity : Natural);
+      Free : not null access procedure (Object : Address));
+
+end System.Reference_Counting;
