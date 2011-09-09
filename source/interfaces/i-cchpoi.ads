38482fb3a75071caa28772dc05d9957a702c7e83
yt <demoonlit@panathenaia.halfmoon.jp>
2011-09-09 19:04:59 +0900

    Delete unnecessary package names

--- source/interfaces/i-cchpoi.ads
+++ source/interfaces/i-cchpoi.ads
@@ -1,7 +1,7 @@
 pragma License (Unrestricted);
 --  extended unit
 with Interfaces.C.Pointers;
-package Interfaces.C.Char_Pointers is new Interfaces.C.Pointers (
+package Interfaces.C.Char_Pointers is new Pointers (
    Index => size_t,
    Element => char,
    Element_Array => char_array,
