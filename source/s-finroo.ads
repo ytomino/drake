7328aa319b0fc6580379fbf74d8e58c205f5155a
yt <demoonlit@panathenaia.halfmoon.jp>
2012-04-28 15:28:22 +0900

    Remove System.Finalization_Root.No_Primitives

--- source/s-finroo.ads
+++ source/s-finroo.ads
@@ -1,6 +1,5 @@
 pragma License (Unrestricted);
 --  implementation unit required by compiler
-with Ada.Streams;
 package System.Finalization_Root is
    pragma Pure;
 
@@ -11,17 +10,5 @@ package System.Finalization_Root is
    procedure Adjust (Object : in out Root_Controlled) is null;
    procedure Initialize (Object : in out Root_Controlled) is null;
    procedure Finalize (Object : in out Root_Controlled) is null;
-
-   package No_Primitives is
-      procedure Write (
-         Stream : not null access Ada.Streams.Root_Stream_Type'Class;
-         Item : Root_Controlled) is null;
-      procedure Read (
-         Stream : not null access Ada.Streams.Root_Stream_Type'Class;
-         Item : out Root_Controlled) is null;
-   end No_Primitives;
-
-   for Root_Controlled'Read use No_Primitives.Read;
-   for Root_Controlled'Write use No_Primitives.Write;
 
 end System.Finalization_Root;
