6fa4677cc99025cbf9ae1715019ba7889ecfd953
yt <demoonlit@panathenaia.halfmoon.jp>
2013-02-04 18:32:44 +0900

    Improve type conversions of Ada.Calendar

--- source/calendar/univ-apple-darwin/a-calins.adb
+++ source/calendar/univ-apple-darwin/a-calins.adb
@@ -31,14 +31,14 @@ package body Ada.Calendar.Inside is
       timespec.tv_sec := timespec.tv_sec + C.sys.types.time_t (Time_Zone) * 60;
       tm := C.time.gmtime_r (timespec.tv_sec'Access, Buffer'Access);
       --  does gmtime_r return no error ?
-      Year := Year_Number (tm.tm_year + 1900);
-      Month := Month_Number (tm.tm_mon + 1);
+      Year := Integer (tm.tm_year) + 1900;
+      Month := Integer (tm.tm_mon) + 1;
       Day := Day_Number (tm.tm_mday);
       Hour := Hour_Number (tm.tm_hour);
       Minute := Minute_Number (tm.tm_min);
       Second := Second_Number (tm.tm_sec);
-      Day_of_Week := Day_Name ((tm.tm_wday + 6) rem 7); -- starts from Monday
       Leap_Second := False;
+      Day_of_Week := (Integer (tm.tm_wday) + 6) rem 7; -- starts from Monday
    end Split;
 
    function Time_Of (
@@ -56,15 +56,15 @@ package body Ada.Calendar.Inside is
          tm_min => 0,
          tm_hour => 0,
          tm_mday => C.signed_int (Day),
-         tm_mon => C.signed_int (Month) - 1,
-         tm_year => C.signed_int (Year) - 1900,
+         tm_mon => C.signed_int (Month_Number'Base (Month) - 1),
+         tm_year => C.signed_int (Year_Number'Base (Year) - 1900),
          tm_wday => 0,
          tm_yday => 0,
          tm_isdst => 0,
          tm_gmtoff => 0,
          tm_zone => null);
       C_Result : constant C.sys.types.time_t := C.time.timegm (tm'Access);
-      Result : Time;
+      Result : Duration;
    begin
       --  UNIX time starts until 1970, Year_Number stats unitl 1901...
       if C_Result = -1 then
@@ -74,9 +74,9 @@ package body Ada.Calendar.Inside is
             raise Time_Error;
          end if;
       else
-         Result := Time (System.Native_Time.To_Time (C_Result));
+         Result := System.Native_Time.To_Time (C_Result);
       end if;
-      return Result - Duration (Time_Zone * 60) + Seconds;
+      return Time (Result - Duration (Time_Zone * 60) + Seconds);
    end Time_Of;
 
 end Ada.Calendar.Inside;
