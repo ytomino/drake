0f7768b319bd4d0877affdd1cd55717ac2dd06b7
yt <demoonlit@panathenaia.halfmoon.jp>
2013-03-07 03:44:36 +0900

    Fix to prevent a exception in an interrupt handler from affecting outside, pass CXC3004

--- source/tasking/univ-apple-darwin/a-intins.adb
+++ source/tasking/univ-apple-darwin/a-intins.adb
@@ -1,7 +1,64 @@
+with Ada.Unchecked_Conversion;
+with Ada.Exceptions;
 with Ada.Interrupts.Names;
+with System.Formatting;
+with System.Termination;
+with System.Unwind;
 with C.signal;
 package body Ada.Interrupts.Inside is
+   use type System.Address;
    use type C.signed_int;
+
+   Report_Traceback : access procedure (
+      Current : Ada.Exceptions.Exception_Occurrence);
+   pragma Import (Ada, Report_Traceback, "__drake_ref_report_traceback");
+   pragma Weak_External (Report_Traceback);
+
+   procedure Report (
+      Interrupt : Interrupt_Id;
+      Current : Exceptions.Exception_Occurrence);
+   procedure Report (
+      Interrupt : Interrupt_Id;
+      Current : Exceptions.Exception_Occurrence)
+   is
+      function Cast is new Unchecked_Conversion (
+         Exceptions.Exception_Occurrence,
+         System.Unwind.Exception_Occurrence);
+      subtype Fixed_String is String (Positive);
+      Full_Name : Fixed_String;
+      for Full_Name'Address use Cast (Current).Id.Full_Name;
+      Name : String (1 .. Interrupt_Id'Width);
+      Name_Last : Natural;
+      Error : Boolean;
+   begin
+      System.Formatting.Image (
+         System.Formatting.Unsigned (Interrupt),
+         Name,
+         Name_Last,
+         Error => Error);
+      System.Termination.Error_New_Line;
+      if Cast (Current).Num_Tracebacks > 0
+         and then Report_Traceback'Address /= System.Null_Address
+      then
+         System.Termination.Error_Put ("Interrupt ");
+         System.Termination.Error_Put (Name (Name'First .. Name_Last));
+         System.Termination.Error_Put (" terminated by unhandled exception");
+         System.Termination.Error_New_Line;
+         Report_Traceback (Current);
+      else
+         System.Termination.Error_Put ("in interrupt ");
+         System.Termination.Error_Put (Name (Name'First .. Name_Last));
+         System.Termination.Error_Put (", raised ");
+         System.Termination.Error_Put (
+            Full_Name (1 .. Cast (Current).Id.Name_Length));
+         if Cast (Current).Msg_Length > 0 then
+            System.Termination.Error_Put (" : ");
+            System.Termination.Error_Put (
+               Cast (Current).Msg (1 .. Cast (Current).Msg_Length));
+         end if;
+         System.Termination.Error_New_Line;
+      end if;
+   end Report;
 
    type Signal_Rec is record
       Installed_Handler : Parameterless_Handler;
@@ -10,8 +67,8 @@ package body Ada.Interrupts.Inside is
    pragma Suppress_Initialization (Signal_Rec);
 
    type Signal_Vec is array (
-      Ada.Interrupts.Names.First_Interrupt_Id ..
-      Ada.Interrupts.Names.Last_Interrupt_Id) of Signal_Rec;
+      Interrupts.Names.First_Interrupt_Id ..
+      Interrupts.Names.Last_Interrupt_Id) of Signal_Rec;
    pragma Suppress_Initialization (Signal_Vec);
 
    Table : Signal_Vec;
@@ -30,6 +87,9 @@ package body Ada.Interrupts.Inside is
       pragma Unreferenced (Context);
    begin
       Table (Interrupt_Id (Signal_Number)).Installed_Handler.all;
+   exception -- CXC3004, an exception propagated from a handler has no effect
+      when E : others =>
+         Report (Interrupt_Id (Signal_Number), E);
    end Handler;
 
    --  implementation
