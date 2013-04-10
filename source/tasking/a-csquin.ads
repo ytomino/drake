15d213de0f5f25517e404b399592cbfead46f7bb
yt <demoonlit@panathenaia.halfmoon.jp>
2013-04-10 10:26:23 +0900

    Implement Unbounded_Synchronized_Queues and Unbounded_Priority_Queues

---
+++ source/tasking/a-csquin.ads
@@ -0,0 +1,27 @@
+pragma License (Unrestricted);
+--  Ada 2012
+generic
+   type Element_Type is private;
+package Ada.Containers.Synchronized_Queue_Interfaces is
+   pragma Pure;
+
+   type Queue is synchronized interface;
+
+   procedure Enqueue (
+      Container : in out Queue;
+      New_Item : Element_Type) is
+      abstract
+      with Synchronization => By_Entry;
+
+   procedure Dequeue (
+      Container : in out Queue;
+      Element : out Element_Type) is
+      abstract
+      with Synchronization => By_Entry;
+
+   function Current_Use (Container : Queue) return Count_Type is
+      abstract;
+   function Peak_Use (Container : Queue) return Count_Type is
+      abstract;
+
+end Ada.Containers.Synchronized_Queue_Interfaces;
---
+++ source/tasking/a-cusyqu.adb
@@ -0,0 +1,49 @@
+package body Ada.Containers.Unbounded_Synchronized_Queues is
+   use type Implementation.Node_Access;
+
+   protected body Queue is
+
+--    overriding
+      entry Enqueue (New_Item : Queue_Interfaces.Element_Type) when True is
+         New_Node : constant Implementation.Node_Access :=
+            new Implementation.Node'(
+               Element => New_Item,
+               Next => null);
+      begin
+         if First = null then
+            First := New_Node;
+         else
+            Last.Next := New_Node;
+         end if;
+         Last := New_Node;
+         Current_Length := Current_Length + 1;
+         if Current_Length > Peak_Length then
+            Peak_Length := Current_Length;
+         end if;
+      end Enqueue;
+
+--    overriding
+      entry Dequeue (Element : out Queue_Interfaces.Element_Type)
+         when First /= null is
+      begin
+         Element := First.Element;
+         First := First.Next;
+         if First = null then
+            Last := null;
+         end if;
+         Current_Length := Current_Length - 1;
+      end Dequeue;
+
+      overriding function Current_Use return Count_Type is
+      begin
+         return Current_Length;
+      end Current_Use;
+
+      overriding function Peak_Use return Count_Type is
+      begin
+         return Peak_Length;
+      end Peak_Use;
+
+   end Queue;
+
+end Ada.Containers.Unbounded_Synchronized_Queues;
