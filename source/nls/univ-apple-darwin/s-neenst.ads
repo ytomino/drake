d20179ed3e024234bef23b2eb1c33a2d67bcae4e
yt <demoonlit@panathenaia.halfmoon.jp>
2013-08-04 01:16:11 +0900

    Add System.Native_Encoding.Encoding_Streams.In_Type and Out_Type

--- source/nls/univ-apple-darwin/s-neenst.ads
+++ source/nls/univ-apple-darwin/s-neenst.ads
@@ -4,6 +4,39 @@ package System.Native_Encoding.Encoding_Streams is
    --  This is a stream wrapper encoding from/to another stream.
    pragma Preelaborate;
 
+   --  only reading
+
+   type In_Type is limited private;
+
+   --  management
+   function Open (
+      Decoder : Converter; -- neither access nor aliased for derived types
+      Stream : not null access Ada.Streams.Root_Stream_Type'Class)
+      return In_Type;
+   function Is_Open (Object : In_Type) return Boolean;
+
+   --  stream access
+   function Stream (Object : aliased in out In_Type)
+      return not null access Ada.Streams.Root_Stream_Type'Class;
+
+   --  only writing
+
+   type Out_Type is limited private;
+
+   --  management
+   function Open (
+      Encoder : Converter; -- same as above
+      Stream : not null access Ada.Streams.Root_Stream_Type'Class)
+      return Out_Type;
+   function Is_Open (Object : Out_Type) return Boolean;
+
+   --  stream access
+   function Stream (Object : aliased in out Out_Type)
+      return not null access Ada.Streams.Root_Stream_Type'Class;
+
+   --  finish writing
+   procedure Finish (Object : in out Out_Type);
+
    --  bidirectional
 
    type Inout_Type is limited private;
@@ -62,6 +95,43 @@ private
       Last : Ada.Streams.Stream_Element_Offset;
    end record;
    pragma Suppress_Initialization (Writing_Context_Type);
+
+   type Converter_Access is access constant Converter;
+   for Converter_Access'Storage_Size use 0;
+
+   --  only reading
+
+   type In_Type is limited new Ada.Streams.Root_Stream_Type with record
+      Stream : Address := Null_Address; -- access Root_Stream_Type'Class;
+      Reading_Converter : Converter_Access;
+      Reading_Context : Reading_Context_Type;
+   end record;
+
+   overriding procedure Read (
+      Object : in out In_Type;
+      Item : out Ada.Streams.Stream_Element_Array;
+      Last : out Ada.Streams.Stream_Element_Offset);
+   overriding procedure Write (
+      Object : in out In_Type;
+      Item : Ada.Streams.Stream_Element_Array)
+      with Import, Convention => Ada, External_Name => "__drake_program_error";
+
+   --  only writing
+
+   type Out_Type is limited new Ada.Streams.Root_Stream_Type with record
+      Stream : Address := Null_Address; -- access Root_Stream_Type'Class;
+      Writing_Converter : Converter_Access;
+      Writing_Context : Writing_Context_Type;
+   end record;
+
+   overriding procedure Read (
+      Object : in out Out_Type;
+      Item : out Ada.Streams.Stream_Element_Array;
+      Last : out Ada.Streams.Stream_Element_Offset)
+      with Import, Convention => Ada, External_Name => "__drake_program_error";
+   overriding procedure Write (
+      Object : in out Out_Type;
+      Item : Ada.Streams.Stream_Element_Array);
 
    --  bidirectional
 
