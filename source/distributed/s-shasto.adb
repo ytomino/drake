d11f4231de959ee6db40dd890643061574220607
yt <demoonlit@panathenaia.halfmoon.jp>
2013-11-12 13:48:49 +0900

    Implement pragma Shared_Passive

---
+++ source/distributed/s-shasto.adb
@@ -0,0 +1,48 @@
+pragma Check_Policy (Trace, Off);
+package body System.Shared_Storage is
+
+   function Nop (Key : String)
+      return access Ada.Streams.Root_Stream_Type'Class
+   is
+      pragma Unreferenced (Key);
+   begin
+      return null;
+   end Nop;
+
+   procedure Shared_Var_Lock (Var : String) is
+   begin
+      pragma Check (Trance, Ada.Debug.Put (Var));
+      Lock_Hook (Var);
+   end Shared_Var_Lock;
+
+   procedure Shared_Var_Unlock (Var : String) is
+   begin
+      pragma Check (Trance, Ada.Debug.Put (Var));
+      Unlock_Hook (Var);
+   end Shared_Var_Unlock;
+
+   package body Shared_Var_Procs is
+
+      procedure Read is
+         pragma Check (Trance, Ada.Debug.Put (Full_Name));
+         Stream : constant access Ada.Streams.Root_Stream_Type'Class :=
+            Read_Hook (Full_Name);
+      begin
+         if Stream /= null then
+            Typ'Read (Stream, V);
+         end if;
+      end Read;
+
+      procedure Write is
+         pragma Check (Trance, Ada.Debug.Put (Full_Name));
+         Stream : constant access Ada.Streams.Root_Stream_Type'Class :=
+            Write_Hook (Full_Name);
+      begin
+         if Stream /= null then
+            Typ'Write (Stream, V);
+         end if;
+      end Write;
+
+   end Shared_Var_Procs;
+
+end System.Shared_Storage;
