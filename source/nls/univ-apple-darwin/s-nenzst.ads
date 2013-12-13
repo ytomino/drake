0033798b911db81ac27c528e3d4bce46cbc1e3fd
yt <demoonlit@panathenaia.halfmoon.jp>
2013-12-14 06:20:35 +0900

    Remark

--- source/nls/univ-apple-darwin/s-nenzst.ads
+++ source/nls/univ-apple-darwin/s-nenzst.ads
@@ -5,4 +5,5 @@ package System.Native_Encoding.Wide_Wide_Strings is
    new Generic_Strings (
       Wide_Wide_Character,
       Wide_Wide_String);
+--  Encoding / decoding between Wide_Wide_String and various encodings.
 pragma Preelaborate (System.Native_Encoding.Wide_Wide_Strings);
--- source/nls/univ-apple-darwin/s-newist.ads
+++ source/nls/univ-apple-darwin/s-newist.ads
@@ -5,4 +5,5 @@ package System.Native_Encoding.Wide_Strings is
    new Generic_Strings (
       Wide_Character,
       Wide_String);
+--  Encoding / decoding between Wide_String and various encodings.
 pragma Preelaborate (System.Native_Encoding.Wide_Strings);
