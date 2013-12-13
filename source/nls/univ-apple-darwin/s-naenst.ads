40681b848734eadb322a76c4ce6d46e9c6a4318d
yt <demoonlit@panathenaia.halfmoon.jp>
2013-12-14 06:20:35 +0900

    Remark

--- source/nls/univ-apple-darwin/s-naenst.ads
+++ source/nls/univ-apple-darwin/s-naenst.ads
@@ -5,4 +5,5 @@ package System.Native_Encoding.Strings is
    new Generic_Strings (
       Character,
       String);
+--  Encoding / decoding between String and various encodings.
 pragma Preelaborate (System.Native_Encoding.Strings);
