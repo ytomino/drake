0ca013700e6d7cdadb6ccaa4dfac5629374abbf5
yt <demoonlit@panathenaia.halfmoon.jp>
2014-02-06 17:08:53 +0900

    Remove the body of the implementation unit System.Assertions

--- source/a-assert.adb
+++ source/a-assert.adb
@@ -1,14 +1,31 @@
-with System.Assertions;
+with System.Unwind;
+pragma Warnings (Off, System.Unwind); -- break "pure" rule
+with System.Unwind.Raising;
+pragma Warnings (Off, System.Unwind.Raising); -- break "pure" rule
 package body Ada.Assertions is
    pragma Suppress (All_Checks);
+
+   --  skip Ada.Exceptions
+   Assertion_Error_Data : aliased constant System.Unwind.Exception_Data;
+   pragma Import (Ada, Assertion_Error_Data, "assertion_error");
+
+   --  implementation
 
    procedure Assert (
       Check : Boolean;
       Message : String := Debug.Source_Location) is
    begin
       if not Check then
-         System.Assertions.Raise_Assert_Failure (Message);
+         Raise_Assertion_Error (Message);
       end if;
    end Assert;
+
+   procedure Raise_Assertion_Error (
+      Message : String := Debug.Source_Location) is
+   begin
+      System.Unwind.Raising.Raise_Exception (
+         Assertion_Error_Data'Access,
+         Message => Message);
+   end Raise_Assertion_Error;
 
 end Ada.Assertions;
