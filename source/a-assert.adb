2777821d41b19a7bef82e2e6d7da69274c97ef2a
yt <demoonlit@panathenaia.halfmoon.jp>
2014-02-28 13:13:07 +0900

    Fix a message raised from pragma Assert

--- source/a-assert.adb
+++ source/a-assert.adb
@@ -1,7 +1,6 @@
-with Ada.Exception_Identification.From_Here;
+with Ada.Exception_Identification;
 package body Ada.Assertions is
    pragma Suppress (All_Checks);
-   use Exception_Identification.From_Here;
 
    --  implementation
 
@@ -17,7 +16,9 @@ package body Ada.Assertions is
    procedure Raise_Assertion_Error (
       Message : String := Debug.Source_Location) is
    begin
-      Raise_Exception (Assertion_Error'Identity, Message => Message);
+      Ada.Exception_Identification.Raise_Exception (
+         Assertion_Error'Identity,
+         Message => Message);
    end Raise_Assertion_Error;
 
 end Ada.Assertions;
