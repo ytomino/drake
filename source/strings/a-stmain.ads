037e6c907c8a1bbeb836d98e91d0d721270b296c
ytomino <demoonlit@panathenaia.halfmoon.jp>
2014-09-03 00:56:06 +0900

    Change Ada.Strings.Wide_Maps.Wide_Character_Set, Wide_Character_Mapping, Ada.Strings.Wide_Wide_Maps.Wide_Wide_Character_Set and Wide_Wide_Character_Mapping from derived types to subtypes of Ada.Strings.Maps.Character_Set and Character_Mapping

--- source/strings/a-stmain.ads
+++ source/strings/a-stmain.ads
@@ -12,23 +12,9 @@ package Ada.Strings.Maps.Inside is
    pragma Inline (To_Set);
 
    generic
-      type T is new Character_Set;
-      with function Source
-         return not null access Characters.Inside.Sets.Character_Set;
-   function To_Derived_Set return T;
-   pragma Inline (To_Derived_Set);
-
-   generic
       with function Source
          return not null access Characters.Inside.Maps.Character_Mapping;
    function To_Mapping return Character_Mapping;
    pragma Inline (To_Mapping);
-
-   generic
-      type T is new Character_Mapping;
-      with function Source
-         return not null access Characters.Inside.Maps.Character_Mapping;
-   function To_Derived_Mapping return T;
-   pragma Inline (To_Derived_Mapping);
 
 end Ada.Strings.Maps.Inside;
