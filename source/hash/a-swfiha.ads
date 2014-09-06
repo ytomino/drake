35618149b2d2b05c04195dfffb0d17fc42ffe511
ytomino <demoonlit@panathenaia.halfmoon.jp>
2014-09-06 16:52:40 +0900

    Insert package names

--- source/hash/a-swfiha.ads
+++ source/hash/a-swfiha.ads
@@ -3,5 +3,5 @@ with Ada.Containers;
 with Ada.Strings.Wide_Hash;
 function Ada.Strings.Wide_Fixed.Hash (Key : Wide_String)
    return Containers.Hash_Type
-   renames Wide_Hash;
+   renames Strings.Wide_Hash;
 pragma Pure (Ada.Strings.Wide_Fixed.Hash);
--- source/hash/a-szfiha.ads
+++ source/hash/a-szfiha.ads
@@ -3,5 +3,5 @@ with Ada.Containers;
 with Ada.Strings.Wide_Wide_Hash;
 function Ada.Strings.Wide_Wide_Fixed.Hash (Key : Wide_Wide_String)
    return Containers.Hash_Type
-   renames Wide_Wide_Hash;
+   renames Strings.Wide_Wide_Hash;
 pragma Pure (Ada.Strings.Wide_Wide_Fixed.Hash);
