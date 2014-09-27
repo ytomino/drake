b671129c4828ca406a78f8eb2c33e73344c6eaf9
ytomino <demoonlit@panathenaia.halfmoon.jp>
2014-09-27 17:35:58 +0900

    Add an iterator into Ada.Command_Line

--- examples/cmdline.adb
+++ examples/cmdline.adb
@@ -3,38 +3,62 @@ with Ada.Command_Line;
 with Ada.Environment_Variables;
 procedure cmdline is
 	Count : Natural;
-	Value_Test : Boolean := False;
 	type String_Access is access String;
 	type Pair is record
 		Name, Value : String_Access;
 	end record;
 	Rec : array (1 .. 255) of Pair;
-	procedure Process (Name, Value : in String) is
-	begin
-		Count := Count + 1;
-		Rec (Count).Name := new String'(Name);
-		Rec (Count).Value := new String'(Value);
-		Ada.Debug.Put (Name & "=" & Value);
-		if Value_Test then
-			if Name = "A" then
-				pragma Assert (Value = "B");
-				null;
-			elsif Name = "C" then
-				pragma Assert (Value = "");
-				null;
-			end if;
-		end if;
-	end Process;
 begin
-	-- iterate command line arguments
 	Ada.Debug.Put (Ada.Command_Line.Command_Name);
+	-- iterate command line arguments
+	Ada.Debug.Put ("*** args(1) ***");
 	for I in 1 .. Ada.Command_Line.Argument_Count loop
 		Ada.Debug.Put (Ada.Command_Line.Argument (I));
 	end loop;
+	-- iterate command line arguments by iterator (forward)
+	Ada.Debug.Put ("*** args(2) ***");
+	declare
+		Count_2 : Natural := 0;
+		Ite : Ada.Command_Line.Iterator_Interfaces.Reversible_Iterator'Class :=
+			Ada.Command_Line.Iterate;
+		Pos : Natural := Ada.Command_Line.Iterator_Interfaces.First (Ite);
+	begin
+		while Ada.Command_Line.Has_Element (Pos) loop
+			Count_2 := Count_2 + 1;
+			pragma Assert (Count_2 = Pos);
+			Pos := Ada.Command_Line.Iterator_Interfaces.Next (Ite, Pos);
+		end loop;
+		pragma Assert (Count_2 = Ada.Command_Line.Argument_Count);
+	end;
+	-- iterate command line arguments by iterator (backward)
+	Ada.Debug.Put ("*** args(3) ***");
+	declare
+		Count_2 : Natural := 0;
+		Ite : Ada.Command_Line.Iterator_Interfaces.Reversible_Iterator'Class :=
+			Ada.Command_Line.Iterate;
+		Pos : Natural := Ada.Command_Line.Iterator_Interfaces.Last (Ite);
+	begin
+		while Ada.Command_Line.Has_Element (Pos) loop
+			pragma Assert (Ada.Command_Line.Argument_Count - Count_2 = Pos);
+			Count_2 := Count_2 + 1;
+			Pos := Ada.Command_Line.Iterator_Interfaces.Previous (Ite, Pos);
+		end loop;
+		pragma Assert (Count_2 = Ada.Command_Line.Argument_Count);
+	end;
 	-- iterate environment variables by closure
 	Ada.Debug.Put ("*** env(1) ***");
 	Count := 0;
-	Ada.Environment_Variables.Iterate (Process'Access);
+	declare
+		procedure Process (Name, Value : in String) is
+		begin
+			Count := Count + 1;
+			Rec (Count).Name := new String'(Name);
+			Rec (Count).Value := new String'(Value);
+			Ada.Debug.Put (Name & "=" & Value);
+		end Process;
+	begin
+		Ada.Environment_Variables.Iterate (Process'Access);
+	end;
 	-- iterate environment variables by iterator
 	Ada.Debug.Put ("*** env(2) ***");
 	declare
@@ -75,7 +99,15 @@ begin
 	begin
 		Ada.Environment_Variables.Clear;
 		Count := 0;
-		Ada.Environment_Variables.Iterate (Process'Access);
+		declare
+			procedure Process (Name, Value : in String) is
+			begin
+				Count := Count + 1;
+				Ada.Debug.Put (Name & "=" & Value);
+			end Process;
+		begin
+			Ada.Environment_Variables.Iterate (Process'Access);
+		end;
 		Cleared_Count := Count;
 		pragma Assert (Cleared_Count = 0
 			or else (
@@ -84,8 +116,22 @@ begin
 		Ada.Environment_Variables.Set ("A", "B");
 		Ada.Environment_Variables.Set ("C", "");
 		Count := 0;
-		Value_Test := True;
-		Ada.Environment_Variables.Iterate (Process'Access);
+		declare
+			procedure Process (Name, Value : in String) is
+			begin
+				Count := Count + 1;
+				Ada.Debug.Put (Name & "=" & Value);
+				if Name = "A" then
+					pragma Assert (Value = "B");
+					null;
+				elsif Name = "C" then
+					pragma Assert (Value = "");
+					null;
+				end if;
+			end Process;
+		begin
+			Ada.Environment_Variables.Iterate (Process'Access);
+		end;
 		pragma Assert (Count = Cleared_Count + 2);
 		pragma Assert (Ada.Environment_Variables.Value ("A") = "B");
 		pragma Assert (Ada.Environment_Variables.Value ("C") = "");
--- source/environment/a-comlin.adb
+++ source/environment/a-comlin.adb
@@ -17,6 +17,22 @@ package body Ada.Command_Line is
       end if;
    end Argument;
 
+   function Has_Element (Position : Natural) return Boolean is
+   begin
+      return Position in 1 .. Argument_Count;
+   end Has_Element;
+
+   function Iterate return Iterator_Interfaces.Reversible_Iterator'Class is
+   begin
+      return Iterate (1, Argument_Count);
+   end Iterate;
+
+   function Iterate (First : Positive; Last : Natural)
+      return Iterator_Interfaces.Reversible_Iterator'Class is
+   begin
+      return Iterator'(First, Last);
+   end Iterate;
+
    function Command_Name return String is
    begin
       return Inside.Argument (0);
@@ -26,5 +42,37 @@ package body Ada.Command_Line is
    begin
       System.Startup.Exit_Status := Integer (Code);
    end Set_Exit_Status;
+
+   --  implementation of the iterator
+
+   overriding function First (Object : Iterator) return Natural is
+   begin
+      return Object.First;
+   end First;
+
+   overriding function Next (Object : Iterator; Position : Natural)
+      return Natural is
+   begin
+      if Position >= Object.Last then
+         return 0;
+      else
+         return Position + 1;
+      end if;
+   end Next;
+
+   overriding function Last (Object : Iterator) return Natural is
+   begin
+      return Object.Last;
+   end Last;
+
+   overriding function Previous (Object : Iterator; Position : Natural)
+      return Natural is
+   begin
+      if Position <= Object.First then
+         return 0;
+      else
+         return Position - 1;
+      end if;
+   end Previous;
 
 end Ada.Command_Line;
