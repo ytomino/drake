8a41173a1a8a3be4bd88960565286a9c9a9dcef7
ytomino <demoonlit@panathenaia.halfmoon.jp>
2014-12-05 17:41:59 +0900

    Change primitives of Ada.Execution_Time.CPU_Time, Real_Time.Time and Time_Span to import intrinsic

--- examples/times.adb
+++ examples/times.adb
@@ -6,6 +6,7 @@ procedure times is
 	use Ada.Text_IO, Duration_IO;
 	use type Ada.Execution_Time.CPU_Time;
 	use type Ada.Real_Time.Time;
+	use type Ada.Real_Time.Time_Span;
 	First_RT : Ada.Real_Time.Time := Ada.Real_Time.Clock;
 	First_CT : Ada.Execution_Time.CPU_Time := Ada.Execution_Time.Clock;
 	Now_RT : Ada.Real_Time.Time;
@@ -16,4 +17,18 @@ begin
 	Now_CT := Ada.Execution_Time.Clock;
 	Put ("Real Time: "); Put (Ada.Real_Time.To_Duration (Now_RT - First_RT)); New_Line;
 	Put ("CPU Time:  "); PUt (Ada.Real_Time.To_Duration (Now_CT - First_CT)); New_Line;
+	-- Time_Span
+	pragma Assert (Ada.Real_Time.To_Time_Span (1.0) + Ada.Real_Time.To_Time_Span (2.0) = Ada.Real_Time.To_Time_Span (3.0));
+	pragma Assert (Ada.Real_Time.To_Time_Span (3.0) - Ada.Real_Time.To_Time_Span (2.0) = Ada.Real_Time.To_Time_Span (1.0));
+	pragma Assert (-Ada.Real_Time.To_Time_Span (1.0) = Ada.Real_Time.To_Time_Span (-1.0));
+	pragma Assert (Ada.Real_Time.To_Time_Span (2.0) * 3 = Ada.Real_Time.To_Time_Span (6.0));
+	pragma Assert (Ada.Real_Time.To_Time_Span (6.0) / Ada.Real_Time.To_Time_Span (2.0) = 3);
+	pragma Assert (Ada.Real_Time.To_Time_Span (6.0) / 2 = Ada.Real_Time.To_Time_Span (3.0));
+	pragma Assert (abs Ada.Real_Time.To_Time_Span (1.0) = Ada.Real_Time.To_Time_Span (1.0));
+	pragma Assert (abs Ada.Real_Time.To_Time_Span (-1.0) = Ada.Real_Time.To_Time_Span (1.0));
+	-- Time and Time_Span
+	pragma Assert (Now_RT + Ada.Real_Time.To_Time_Span (2.0) - Now_RT = Ada.Real_Time.To_Time_Span (2.0));
+	pragma Assert (Ada.Real_Time.To_Time_Span (2.0) + Now_RT = Now_RT + Ada.Real_Time.To_Time_Span (2.0));
+	pragma Assert (Now_RT - Ada.Real_Time.To_Time_Span (2.0) = Now_RT + Ada.Real_Time.To_Time_Span (-2.0));
+	pragma Debug (Ada.Debug.Put ("OK"));
 end times;
--- source/calendar/a-exetim.adb
+++ source/calendar/a-exetim.adb
@@ -5,10 +5,4 @@ package body Ada.Execution_Time is
    function Clock return CPU_Time
       renames Inside.Clock;
 
-   function "-" (Left : CPU_Time; Right : CPU_Time)
-      return Real_Time.Time_Span is
-   begin
-      return Real_Time."-" (Real_Time.Time (Left), Real_Time.Time (Right));
-   end "-";
-
 end Ada.Execution_Time;
