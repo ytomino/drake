ad12ab2829c570a9cbfda87df6ed93a25685ac92
ytomino <demoonlit@panathenaia.halfmoon.jp>
2015-03-08 19:06:05 +0900

    Reformat

--- source/streams/a-ssiopi.adb
+++ source/streams/a-ssiopi.adb
@@ -6,16 +6,8 @@ package body Ada.Streams.Stream_IO.Pipes is
       Handles : array (0 .. 1) of aliased System.Native_IO.Handle_Type;
    begin
       System.Native_IO.Open_Pipe (Handles (0), Handles (1));
-      Naked.Open (
-            Reading,
-            In_File,
-            Handles (0),
-            To_Close => True);
-      Naked.Open (
-            Writing,
-            Out_File,
-            Handles (1),
-            To_Close => True);
+      Naked.Open (Reading, In_File, Handles (0), To_Close => True);
+      Naked.Open (Writing, Out_File, Handles (1), To_Close => True);
    end Create;
 
 end Ada.Streams.Stream_IO.Pipes;
