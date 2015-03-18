bdd4ce03b062865c4c5aad26a86d5c0ac7a9614a
ytomino <demoonlit@panathenaia.halfmoon.jp>
2015-03-18 10:27:46 +0900

    Add Ada.Strings.Generic_Unbounded.Reserve_Capacity

--- source/strings/a-stgeun.adb
+++ source/strings/a-stgeun.adb
@@ -152,27 +152,6 @@ package body Ada.Strings.Generic_Unbounded is
       Target := Upcast (Data);
    end Copy_Data;
 
-   procedure Reserve_Capacity (
-      Item : in out Unbounded_String;
-      Capacity : Natural);
-   procedure Reserve_Capacity (
-      Item : in out Unbounded_String;
-      Capacity : Natural)
-   is
-      New_Capacity : constant Natural := Integer'Max (Capacity, Item.Length);
-   begin
-      System.Reference_Counting.Unique (
-         Target => Upcast (Item.Data'Unchecked_Access),
-         Target_Length => System.Reference_Counting.Length_Type (Item.Length),
-         Target_Capacity => System.Reference_Counting.Length_Type (
-            Generic_Unbounded.Capacity (Item)),
-         Max_Length => System.Reference_Counting.Length_Type (Item.Length),
-         Capacity => System.Reference_Counting.Length_Type (New_Capacity),
-         Sentinel => Upcast (Empty_Data'Unrestricted_Access),
-         Copy => Copy_Data'Access,
-         Free => Free_Data'Access);
-   end Reserve_Capacity;
-
    procedure Unique (Item : in out Unbounded_String);
    procedure Unique (Item : in out Unbounded_String) is
    begin
@@ -238,6 +217,24 @@ package body Ada.Strings.Generic_Unbounded is
    begin
       return Source.Data.Items'Last;
    end Capacity;
+
+   procedure Reserve_Capacity (
+      Item : in out Unbounded_String;
+      Capacity : Natural)
+   is
+      New_Capacity : constant Natural := Integer'Max (Capacity, Item.Length);
+   begin
+      System.Reference_Counting.Unique (
+         Target => Upcast (Item.Data'Unchecked_Access),
+         Target_Length => System.Reference_Counting.Length_Type (Item.Length),
+         Target_Capacity => System.Reference_Counting.Length_Type (
+            Generic_Unbounded.Capacity (Item)),
+         Max_Length => System.Reference_Counting.Length_Type (Item.Length),
+         Capacity => System.Reference_Counting.Length_Type (New_Capacity),
+         Sentinel => Upcast (Empty_Data'Unrestricted_Access),
+         Copy => Copy_Data'Access,
+         Free => Free_Data'Access);
+   end Reserve_Capacity;
 
    function To_Unbounded_String (Source : String_Type)
       return Unbounded_String
--- source/strings/a-stgeun.ads
+++ source/strings/a-stgeun.ads
@@ -39,6 +39,9 @@ package Ada.Strings.Generic_Unbounded is
    procedure Set_Length (Source : in out Unbounded_String; Length : Natural);
    function Capacity (Source : Unbounded_String'Class) return Natural;
    pragma Inline (Capacity);
+   procedure Reserve_Capacity (
+      Item : in out Unbounded_String;
+      Capacity : Natural);
 
    type String_Access is access all String_Type;
 --  procedure Free (X : in out String_Access);
