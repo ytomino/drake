b5fcbed2dc6200cf12424a726fd3af3e79bdeb58
ytomino <demoonlit@panathenaia.halfmoon.jp>
2015-04-01 19:09:22 +0900

    Fix renamings of library units to be real renamings, for gcc-5

--- source/ada83/unchconv.ads
+++ source/ada83/unchconv.ads
@@ -1,10 +1,3 @@
 pragma License (Unrestricted);
---  with Ada.Unchecked_Conversion;
-generic
-   type Source (<>) is limited private;
-   type Target (<>) is limited private;
-function Unchecked_Conversion (S : Source) return Target;
---  renames Ada.Unchecked_Conversion;
---  generic_renaming_declaration could not be used... (?)
-pragma Pure (Unchecked_Conversion);
-pragma Import (Intrinsic, Unchecked_Conversion);
+with Ada.Unchecked_Conversion;
+generic function Unchecked_Conversion renames Ada.Unchecked_Conversion;
