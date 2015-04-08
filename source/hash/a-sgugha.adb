135c0e4a1ac5a7cb8d20c206bab179529380c27f
ytomino <demoonlit@panathenaia.halfmoon.jp>
2015-04-09 07:23:38 +0900

    Remove constraints from the header block of Ada.Strings.Unbounded

--- source/hash/a-sgugha.adb
+++ source/hash/a-sgugha.adb
@@ -1,5 +1,7 @@
 function Ada.Strings.Generic_Unbounded.Generic_Hash (Key : Unbounded_String)
-   return Ada.Containers.Hash_Type is
+   return Ada.Containers.Hash_Type
+is
+   pragma Suppress (Access_Check);
 begin
    return Fixed_Hash (Key.Data.Items (1 .. Key.Length));
 end Ada.Strings.Generic_Unbounded.Generic_Hash;
