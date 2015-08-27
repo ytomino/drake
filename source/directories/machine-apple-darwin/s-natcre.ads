94fba6f23038254b3f86ec5009fce87f5ca9616b
ytomino <demoonlit@panathenaia.halfmoon.jp>
2015-08-28 02:21:33 +0900

    Fix System.Native_Credentials.Group_Name in POSIX

--- source/directories/machine-apple-darwin/s-natcre.ads
+++ source/directories/machine-apple-darwin/s-natcre.ads
@@ -10,8 +10,10 @@ package System.Native_Credentials is
 
    function Current_User return User_Id
       renames C.unistd.getuid;
+   function Current_Group return Group_Id
+      renames C.unistd.getgid;
 
    function User_Name (Id : User_Id := Current_User) return String;
-   function Group_Name (Id : Group_Id := Current_User) return String;
+   function Group_Name (Id : Group_Id := Current_Group) return String;
 
 end System.Native_Credentials;
