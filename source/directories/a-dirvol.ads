418b2401bec0adac97f08a8a867c2bd7c2695f22
ytomino <demoonlit@panathenaia.halfmoon.jp>
2015-09-13 19:48:44 +0900

    Remark Assigned_File_System into Ada.Directories.Volumes

--- source/directories/a-dirvol.ads
+++ source/directories/a-dirvol.ads
@@ -6,26 +6,51 @@ package Ada.Directories.Volumes is
 
    type File_System is limited private;
 
+--  subtype Assigned_File_System is File_System
+--    with
+--       Dynamic_Predicate => Is_Assigned (Assigned_File_System),
+--       Predicate_Failure => raise Status_Error;
+
    function Is_Assigned (FS : File_System) return Boolean;
 
    function Where (Name : String) return File_System;
 
-   function Size (FS : File_System) return File_Size;
-   function Free_Space (FS : File_System) return File_Size;
+   function Size (
+      FS : File_System) -- Assigned_File_System
+      return File_Size;
+   function Free_Space (
+      FS : File_System) -- Assigned_File_System
+      return File_Size;
 
-   function Owner (FS : File_System) return String;
-   function Format_Name (FS : File_System) return String;
-   function Directory (FS : File_System) return String; -- mounted to
-   function Device (FS : File_System) return String; -- mounted from
+   function Owner (
+      FS : File_System) -- Assigned_File_System
+      return String;
+   function Format_Name (
+      FS : File_System) -- Assigned_File_System
+      return String;
+   function Directory (
+      FS : File_System) -- Assigned_File_System
+      return String; -- mounted to
+   function Device (
+      FS : File_System) -- Assigned_File_System
+      return String; -- mounted from
 
-   function Case_Preserving (FS : File_System) return Boolean;
-   function Case_Sensitive (FS : File_System) return Boolean;
+   function Case_Preserving (
+      FS : File_System) -- Assigned_File_System
+      return Boolean;
+   function Case_Sensitive (
+      FS : File_System) -- Assigned_File_System
+      return Boolean;
 
-   function Is_HFS (FS : File_System) return Boolean;
+   function Is_HFS (
+      FS : File_System) -- Assigned_File_System
+      return Boolean;
 
    type File_System_Id is private;
 
-   function Identity (FS : File_System) return File_System_Id;
+   function Identity (
+      FS : File_System) -- Assigned_File_System
+      return File_System_Id;
 
 private
 
