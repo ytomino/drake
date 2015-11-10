68d0f6871159e583e0322586b72de6f614909310
ytomino <demoonlit@panathenaia.halfmoon.jp>
2015-11-10 11:34:44 +0900

    Fix System.Native_Allocators.Reallocate to round up the size in Windows

--- examples/heap.adb
+++ examples/heap.adb
@@ -3,9 +3,6 @@ with System.Formatting;
 with System.Native_Allocators.Allocated_Size;
 with System.Storage_Elements;
 procedure heap is
-	type Integer_Access is access all Integer;
-	procedure Free is new Ada.Unchecked_Deallocation (Integer, Integer_Access);
-	x, y : Integer_Access;
 	procedure Put (X : System.Storage_Elements.Integer_Address) is
 		S : String (1 .. Standard'Address_Size / 4);
 		Last : Natural;
@@ -35,12 +32,29 @@ procedure heap is
 		Ada.Debug.Put (S);
 	end Put;
 begin
-	x := new Integer;
-	y := new Integer;
-	Put (System.Storage_Elements.To_Integer (x'Pool_Address));
-	Put (System.Native_Allocators.Allocated_Size (x'Pool_Address));
-	Put (System.Storage_Elements.To_Integer (y'Pool_Address));
-	Put (System.Native_Allocators.Allocated_Size (y'Pool_Address));
-	Free (x);
-	Free (y);
+	declare -- new and Unchecked_Deallocation
+		type Integer_Access is access all Integer;
+		procedure Free is new Ada.Unchecked_Deallocation (Integer, Integer_Access);
+		x, y : Integer_Access;
+	begin
+		x := new Integer;
+		y := new Integer;
+		Put (System.Storage_Elements.To_Integer (x'Pool_Address));
+		Put (System.Native_Allocators.Allocated_Size (x'Pool_Address));
+		Put (System.Storage_Elements.To_Integer (y'Pool_Address));
+		Put (System.Native_Allocators.Allocated_Size (y'Pool_Address));
+		Free (x);
+		Free (y);
+	end;
+	declare -- low-level
+		x : System.Address;
+	begin
+		x := System.Native_Allocators.Allocate (3);
+		Put (System.Storage_Elements.To_Integer (x));
+		Put (System.Native_Allocators.Allocated_Size (x));
+		x := System.Native_Allocators.Reallocate (x, 33);
+		Put (System.Storage_Elements.To_Integer (x));
+		Put (System.Native_Allocators.Allocated_Size (x));
+		System.Native_Allocators.Free (x);
+	end;
 end heap;
