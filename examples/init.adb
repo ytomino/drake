5c981897cd0d557f14277b0296ccdc10f5da1d09
ytomino <demoonlit@panathenaia.halfmoon.jp>
2016-01-10 04:58:53 +0900

    Change System.Initialization not to set alignment, and reduce the size of Object_Storage

--- examples/init.adb
+++ examples/init.adb
@@ -1,5 +1,6 @@
 with Ada.Finalization;
 with System.Initialization;
+with System.Storage_Elements;
 procedure init is
 	use type System.Address;
 begin
@@ -40,18 +41,26 @@ begin
 		
 		Another_Object : Dummy := (Ada.Finalization.Controlled with null record);
 		
+		Alignment_Of_Dummy : constant :=
+			System.Storage_Elements.Integer_Address'Alignment;
+			-- Dummy'Alignment; -- is not static.
 		package I is new System.Initialization (Dummy);
-		Storage : aliased I.Object_Storage;
+		type Object_Storage is new I.Object_Storage;
+		for Object_Storage'Alignment use Alignment_Of_Dummy;
+		
+		Storage : aliased Object_Storage;
 		P : access Dummy;
 	begin
+		pragma Assert (Object_Storage'Size = Dummy'Size);
+		pragma Assert (Object_Storage'Alignment = Dummy'Alignment);
 		The_Address := Storage'Address;
-		P := I.New_Object (Storage'Access);
+		P := New_Object (Storage'Access);
 		pragma Assert (Phase = Initialized);
 		pragma Assert (Adjust_Count = 0);
 		P.all := Another_Object;
 		pragma Assert (Phase = Initialized);
 		pragma Assert (Adjust_Count = 1);
-		I.Dispose_Object (Storage'Access);
+		Dispose_Object (Storage'Access);
 		pragma Assert (Phase = Finalized);
 		
 		In_Another := True; -- for finalizing Anotehr_Object
@@ -61,13 +70,20 @@ begin
 		type T is record
 			F : Integer := 123;
 		end record;
+		
+		Alignment_Of_T : constant := Integer'Alignment;
 		package J is new System.Initialization (T);
-		Storage : aliased J.Object_Storage;
+		type Object_Storage is new J.Object_Storage;
+		for Object_Storage'Alignment use Alignment_Of_T;
+		
+		Storage : aliased Object_Storage;
 		P : access T;
 	begin
-		P := J.New_Object (Storage'Access);
+		pragma Assert (Object_Storage'Size = T'Size);
+		pragma Assert (Object_Storage'Alignment = T'Alignment);
+		P := New_Object (Storage'Access);
 		pragma Assert (P.all = (F => 123));
-		P := J.New_Object (Storage'Access, (F => 456));
+		P := New_Object (Storage'Access, (F => 456));
 		pragma Assert (P.all = (F => 456));
 	end;
 	pragma Debug (Ada.Debug.Put ("OK"));
