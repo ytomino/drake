f92878f0c1d50998cf752785441abdd9d2221ea1
ytomino <demoonlit@panathenaia.halfmoon.jp>
2016-03-12 16:24:26 +0900

    Move Index_Type to the front of Element_Type

--- source/required/s-ststop.ads
+++ source/required/s-ststop.ads
@@ -24,16 +24,16 @@ package System.Strings.Stream_Ops is
    --  required for String'Input by compiler (s-ststop.ads)
    function String_Input_Blk_IO is
       new Ada.Streams.Block_Transmission.Input (
-         Character,
          Positive,
+         Character,
          String,
          String_Read_Blk_IO);
 
    --  required for String'Output by compiler (s-ststop.ads)
    procedure String_Output_Blk_IO is
       new Ada.Streams.Block_Transmission.Output (
-         Character,
          Positive,
+         Character,
          String,
          String_Write_Blk_IO);
 
@@ -52,16 +52,16 @@ package System.Strings.Stream_Ops is
    --  required for Wide_String'Input by compiler (s-ststop.ads)
    function Wide_String_Input_Blk_IO is
       new Ada.Streams.Block_Transmission.Input (
-         Wide_Character,
          Positive,
+         Wide_Character,
          Wide_String,
          Wide_String_Read_Blk_IO);
 
    --  required for Wide_String'Output by compiler (s-ststop.ads)
    procedure Wide_String_Output_Blk_IO is
       new Ada.Streams.Block_Transmission.Output (
-         Wide_Character,
          Positive,
+         Wide_Character,
          Wide_String,
          Wide_String_Write_Blk_IO);
 
@@ -80,46 +80,46 @@ package System.Strings.Stream_Ops is
    --  required for Wide_Wide_String'Input by compiler (s-ststop.ads)
    function Wide_Wide_String_Input_Blk_IO is
       new Ada.Streams.Block_Transmission.Input (
-         Wide_Wide_Character,
          Positive,
+         Wide_Wide_Character,
          Wide_Wide_String,
          Wide_Wide_String_Read_Blk_IO);
 
    --  required for Wide_Wide_String'Output by compiler (s-ststop.ads)
    procedure Wide_Wide_String_Output_Blk_IO is
       new Ada.Streams.Block_Transmission.Output (
-         Wide_Wide_Character,
          Positive,
+         Wide_Wide_Character,
          Wide_Wide_String,
          Wide_Wide_String_Write_Blk_IO);
 
    --  required for System.Storage_Elements.Storage_Array'Read (s-ststop.ads)
    procedure Storage_Array_Read_Blk_IO is
       new Ada.Streams.Block_Transmission.Read (
-         Storage_Elements.Storage_Element,
          Storage_Elements.Storage_Offset,
+         Storage_Elements.Storage_Element,
          Storage_Elements.Storage_Array);
 
    --  required for System.Storage_Elements.Storage_Array'Write (s-ststop.ads)
    procedure Storage_Array_Write_Blk_IO is
       new Ada.Streams.Block_Transmission.Write (
-         Storage_Elements.Storage_Element,
          Storage_Elements.Storage_Offset,
+         Storage_Elements.Storage_Element,
          Storage_Elements.Storage_Array);
 
    --  required for System.Storage_Elements.Storage_Array'Input (s-ststop.ads)
    function Storage_Array_Input_Blk_IO is
       new Ada.Streams.Block_Transmission.Input (
-         Storage_Elements.Storage_Element,
          Storage_Elements.Storage_Offset,
+         Storage_Elements.Storage_Element,
          Storage_Elements.Storage_Array,
          Storage_Array_Read_Blk_IO);
 
    --  required for System.Storage_Elements.Storage_Array'Output (s-ststop.ads)
    procedure Storage_Array_Output_Blk_IO is
       new Ada.Streams.Block_Transmission.Output (
-         Storage_Elements.Storage_Element,
          Storage_Elements.Storage_Offset,
+         Storage_Elements.Storage_Element,
          Storage_Elements.Storage_Array,
          Storage_Array_Write_Blk_IO);
 
@@ -138,16 +138,16 @@ package System.Strings.Stream_Ops is
    --  required for Ada.Streams.Stream_Element_Array'Input (s-ststop.ads)
    function Stream_Element_Array_Input_Blk_IO is
       new Ada.Streams.Block_Transmission.Input (
-         Ada.Streams.Stream_Element,
          Ada.Streams.Stream_Element_Offset,
+         Ada.Streams.Stream_Element,
          Ada.Streams.Stream_Element_Array,
          Stream_Element_Array_Read_Blk_IO);
 
    --  required for Ada.Streams.Stream_Element_Array'Output (s-ststop.ads)
    procedure Stream_Element_Array_Output_Blk_IO is
       new Ada.Streams.Block_Transmission.Output (
-         Ada.Streams.Stream_Element,
          Ada.Streams.Stream_Element_Offset,
+         Ada.Streams.Stream_Element,
          Ada.Streams.Stream_Element_Array,
          Stream_Element_Array_Write_Blk_IO);
 
