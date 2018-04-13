c64e355a3897ebf05fdb6e9067a859eb2296e9e8
ytomino <demoonlit@panathenaia.halfmoon.jp>
2018-04-13 16:47:57 +0900

    Share same code for Integer'Image and Long_Long_Integer'Image in 64bit

--- source/image/required/s-imglli.adb
+++ source/image/required/s-imglli.adb
@@ -1,14 +1,17 @@
+with System.Img_Int;
 with System.Formatting;
 with System.Long_Long_Integer_Types;
 package body System.Img_LLI is
    use type Long_Long_Integer_Types.Long_Long_Unsigned;
 
-   subtype Word_Unsigned is Long_Long_Integer_Types.Word_Unsigned;
+   subtype Word_Integer is Long_Long_Integer_Types.Word_Integer;
    subtype Long_Long_Unsigned is Long_Long_Integer_Types.Long_Long_Unsigned;
 
-   --  implementation
-
-   procedure Image_Long_Long_Integer (
+   procedure Image (
+      V : Long_Long_Integer;
+      S : in out String;
+      P : out Natural);
+   procedure Image (
       V : Long_Long_Integer;
       S : in out String;
       P : out Natural)
@@ -24,13 +27,22 @@ package body System.Img_LLI is
          S (S'First) := ' ';
          X := Long_Long_Unsigned (V);
       end if;
-      if Long_Long_Integer'Size <= Standard'Word_Size then
-         Formatting.Image (Word_Unsigned (X), S (S'First + 1 .. S'Last), P,
-            Error => Error);
-      else
-         Formatting.Image (X, S (S'First + 1 .. S'Last), P, Error => Error);
-      end if;
+      Formatting.Image (X, S (S'First + 1 .. S'Last), P, Error => Error);
       pragma Assert (not Error);
+   end Image;
+
+   --  implementation
+
+   procedure Image_Long_Long_Integer (
+      V : Long_Long_Integer;
+      S : in out String;
+      P : out Natural) is
+   begin
+      if Long_Long_Integer'Size <= Standard'Word_Size then
+         Img_Int.Image (Word_Integer (V), S, P);
+      else
+         Image (V, S, P);
+      end if;
    end Image_Long_Long_Integer;
 
 end System.Img_LLI;
--- source/image/required/s-imguns.adb
+++ source/image/required/s-imguns.adb
@@ -1,5 +1,4 @@
 with System.Formatting;
-with System.Long_Long_Integer_Types;
 package body System.Img_Uns is
 
    subtype Word_Unsigned is Long_Long_Integer_Types.Word_Unsigned;
@@ -9,18 +8,22 @@ package body System.Img_Uns is
    procedure Image_Unsigned (
       V : Unsigned_Types.Unsigned;
       S : in out String;
+      P : out Natural) is
+   begin
+      Image (Word_Unsigned (V), S, P);
+   end Image_Unsigned;
+
+   procedure Image (
+      V : Long_Long_Integer_Types.Word_Unsigned;
+      S : in out String;
       P : out Natural)
    is
       Error : Boolean;
    begin
       pragma Assert (S'Length >= 1);
       S (S'First) := ' ';
-      Formatting.Image (
-         Word_Unsigned (V),
-         S (S'First + 1 .. S'Last),
-         P,
-         Error => Error);
+      Formatting.Image (V, S (S'First + 1 .. S'Last), P, Error => Error);
       pragma Assert (not Error);
-   end Image_Unsigned;
+   end Image;
 
 end System.Img_Uns;
